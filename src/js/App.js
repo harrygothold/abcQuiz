b18e4526c5063c93633472c2efd39e44457eba24
Harry Gothold <harrygot@hotmail.com>
2019-01-28 23:57:55 +0000

    added question component

--- src/js/App.js
+++ src/js/App.js
@@ -1,88 +1,81 @@
 import React from 'react';
+import data from './data/Data';
+import Question from './Question';
 
 class App extends React.Component {
 
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      allQuestions: data.allQuestions,
+      currentQuestion: data.allQuestions[0],
+      progress: 0,
+      allAnswers: []
     }
-    render(){
-        return (
-            <div>
-                  
-              {/* Header - start */}
-              <header>
-                  <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/images/plane.svg" />
-              </header>
-              {/* Header - end */}
 
-              {/* Content - start */}
-              <div className={`content`}>
+  }
+  render() {
+    const { currentQuestion } = this.state;
+    return (
+      <div>
 
-                {/* Progress - start */}
-                <div className="progress-container">
-                  <div className="progress-label">1 of 5 answered</div>
-                  <div className="progress">
-                    <div className="progress-bar" style={{'width': `20%`}}>
-                      <span className="sr-only">20% Complete</span>
-                    </div>
-                  </div>
-                </div>
-                {/* Progress - end */}
+        {/* Header - start */}
+        <header>
+          <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/images/plane.svg" />
+        </header>
+        {/* Header - end */}
 
-                {/* Question - start */}
-                <div className={`question`}>
+        {/* Content - start */}
+        <div className={`content`}>
 
-                  <h1>What is the best city in the world?</h1>
-
-                  {/* Choices - start */}
-                  <div className="choices">
-                    
-                    {/* Buttons - start */}
-                    <button className="btn btn-huge is-selected"><span className="letter">A</span> Melbourne</button>
-                    <button className="btn btn-huge"><span className="letter">B</span> New York</button>
-                    <button className="btn btn-huge"><span className="letter">C</span> London</button>
-                    {/* Buttons - end */}
-
-                  </div>
-                  {/* Choices - end */}
-
-                </div>
-                {/* Question - end */}
-
-                {/* Results - start */}
-                <div className="results">
-                  <div className="loader"><div className="icon"></div></div>
-                  <div className="results-overlay"></div>
-                  <h1>Here are your answers:</h1>
-                  <div className="answers">
-                    <ol>
-                      <li>What is the best city in the world? <br /><strong>Melbourne</strong></li>
-                    </ol>
-                  </div>
-                  <div className="text-center">
-                    <button className="btn btn-dark">Submit</button>
-                  </div>
-                </div>
-                {/* Results - end */}
-
+          {/* Progress - start */}
+          <div className="progress-container">
+            <div className="progress-label">1 of 5 answered</div>
+            <div className="progress">
+              <div className="progress-bar" style={{ 'width': `20%` }}>
+                <span className="sr-only">20% Complete</span>
               </div>
-              {/* Content - end */}
-
-              {/* Navigation - start */}
-              <div className={`navigation text-center is-active`}>
-                <button className={`arrow`}>
-                    <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-left-arrow.svg" />
-                </button>
-                <button disabled className={`arrow is-disabled`}>
-                    <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-right-arrow.svg" />
-                </button>
-              </div>
-              {/* Navigation - end */}
-
             </div>
-        )
-    }
+          </div>
+          {/* Progress - end */}
+
+          <Question currentQuestion={currentQuestion} />
+
+
+          {/* Results - start */}
+          <div className="results">
+            <div className="loader"><div className="icon"></div></div>
+            <div className="results-overlay"></div>
+            <h1>Here are your answers:</h1>
+            <div className="answers">
+              <ol>
+                <li>What is the best city in the world? <br /><strong>Melbourne</strong></li>
+              </ol>
+            </div>
+            <div className="text-center">
+              <button className="btn btn-dark">Submit</button>
+            </div>
+          </div>
+          {/* Results - end */}
+
+        </div>
+        {/* Content - end */}
+
+        {/* Navigation - start */}
+        <div className={`navigation text-center is-active`}>
+          <button className={`arrow`}>
+            <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-left-arrow.svg" />
+          </button>
+          <button disabled className={`arrow is-disabled`}>
+            <img src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-right-arrow.svg" />
+          </button>
+        </div>
+        {/* Navigation - end */}
+
+      </div>
+    )
+  }
 }
 
 export default App;
